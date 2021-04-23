f4d82df956d96085159fb1d750932ad6c0e38be0
Nathan <nathanbn123@gmail.com>
2021-04-23 12:50:52 -0700

    full board functionality

--- src/childBoxes.js
+++ src/childBoxes.js
@@ -3,44 +3,6 @@ import React, { Component, useState } from 'react'
 
 function ChildBoxes() {
  
-//   const boxFlip = () =>{
-//         if(this.state.position !== 'row') {
-//            this.setState({
-//             position: "column"
-//            })
-            
-//         } else {
-//             this.setState({
-//                 position: "row"
-//                })
-//         }
-//     }
-
-
-
-
-//    const boxClick = () =>{
-//         this.setState({
-        
-//             color: ["blue", "white", "yellow", "red"]
-//         })
-//     }
-
- 
-
-
-    // let positionStyle= {
-
-    // }
-
-    // let rowStyle = {
-    //     flexDirection: 'row'
-    // }
-
-    // let colStyle = {
-    //     flexDirection: "column"
-    // }
-
     const boxes = [];
     
     const colors = ['red', 'white', 'blue', 'yellow']
@@ -48,31 +10,12 @@ function ChildBoxes() {
     let [colPosition, colSwitch] = useState("flexColChild")
     let [rowPosition, rowSwitch] = useState("flexRowChild")
 
+    
+
     const getRandomNumber = (max) => {
         return Math.floor(Math.random() *max)
     }
 
-    
-   const  makeBoxes= ()=>{
-    for (let i = 0; i<60; i++){
-        boxes.push(
-            <div>
-            <div className={colPosition} onClick={() => colFlip()}>
-            <div className="flexGrandChild" style={{backgroundColor: colors[getRandomNumber(4)]}}/>
-            <div className="flexGrandChild" style={{ backgroundColor: colors[getRandomNumber(4)],}}/>
-           
-          </div>
-          
-          <div className={rowPosition} onClick={() => rowFlip()}>
-            <div className="flexColGrandChild" style={{ backgroundColor: colors[getRandomNumber(4)],}}/>
-            <div className="flexColGrandChild" style={{ backgroundColor: colors[getRandomNumber(4)],}}/>
-          </div>
-          </div>
-
-        )
-    }
-
-    }
 
     function colFlip() {
         if(colPosition === "flexRowChild") {
@@ -92,17 +35,32 @@ function ChildBoxes() {
       
     }
 
-        makeBoxes()
+    const  makeBoxes= ()=>{
+        for (let i = 0; i<60; i++){
+            boxes.push(
+                <div>
+                <div className={colPosition} onClick={() => colFlip()}>
+                <div className="flexGrandChild" style={{backgroundColor: colors[getRandomNumber(4)]}}/>
+                <div className="flexGrandChild" style={{ backgroundColor: colors[getRandomNumber(4)],}}/>
+               
+              </div>
+              
+              <div className={rowPosition} onClick={() => rowFlip()}>
+                <div className="flexColGrandChild" style={{ backgroundColor: colors[getRandomNumber(4)],}}/>
+                <div className="flexColGrandChild" style={{ backgroundColor: colors[getRandomNumber(4)],}}/>
+              </div>
+              </div>
+    
+                 
+            )}
+    
+        }
+
+    makeBoxes()
     return (
        
-        <div className="flexContainer">
-        
+    <div className="flexContainer">
         {boxes}
-       
-
-
-
-
       </div>
 
         )
